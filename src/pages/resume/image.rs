bda85c2329273bd6bb133210f4d0b0b2415026e5
s1n7ax <srineshnisala@gmail.com>
2022-05-21 14:45:44 +0530

    refactor: restructure resume

--- src/pages/resume/image.rs
+++ src/pages/resume/image.rs
@@ -5,6 +5,25 @@ pub fn image() -> Html {
     let src = format!("assets/images/me_01.webp");
 
     html! {
-        <img class={classes!("w-96")} src={src} />
+        <div class={classes!()}>
+            <svg class={classes!()} viewBox="15 15 150 150">
+                <defs>
+                    <pattern
+                        id="imgpattern"
+                        width="1"
+                        height="1"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="xMidYMid slice"
+                    >
+                        <image width="100" height="100" href={ src }/>
+                    </pattern>
+                </defs>
+
+                <path
+                    fill="url(#imgpattern)"
+                    d="M36.7,-43.4C41.7,-31.6,36,-15.8,36.5,0.5C37,16.8,43.7,33.6,38.7,40.8C33.6,47.9,16.8,45.4,-1,46.4C-18.8,47.4,-37.5,51.8,-48.7,44.7C-59.8,37.5,-63.3,18.8,-61,2.3C-58.7,-14.2,-50.6,-28.4,-39.5,-40.2C-28.4,-52,-14.2,-61.4,0.8,-62.2C15.8,-63,31.6,-55.2,36.7,-43.4Z" transform="translate(100 100)"
+                />
+            </svg>
+        </div>
     }
 }
--- src/pages/resume/resume.rs
+++ src/pages/resume/resume.rs
@@ -10,17 +10,25 @@ pub fn resume() -> Html {
     html! {
         <Body>
             <Container>
-                <div class={classes!("grid", "grid-cols-3", "gap-3", "w-full")}>
-                    <div class={classes!("bg-red-400")}><Title /></div>
-                    <div class={classes!("bg-red-400")}><Image /></div>
-                    <div class={classes!("bg-red-400")}><Contact /></div>
-                    <div class={classes!("bg-red-400")}><Profile /></div>
-                    <div class={classes!("bg-red-400")}><Experience /></div>
-                    <div class={classes!("bg-red-400")}><Education /></div>
-                    <div class={classes!("bg-red-400")}><Awards /></div>
-                    <div class={classes!("bg-red-400")}><Technologies /></div>
+                <div
+                    class={
+                        classes!(
+                            "grid",
+                            "grid-cols-1",
+                            "lg:grid-cols-10",
+                            "gap-3")
+                    }>
+                    <div class={classes!("lg:col-span-4", "lg:row-span-2", "bg-red-400")}><Title /></div>
+                    <div class={classes!("lg:col-span-6", "lg:row-span-3", "bg-red-400")}><Image /></div>
+                    <div class={classes!("lg:col-span-4", "bg-red-400")}><Contact /></div>
+                    <div class={classes!("lg:col-span-10", "bg-red-400")}><Profile /></div>
+                    <div class={classes!("lg:col-span-5", "bg-red-400")}><Experience /></div>
+                    <div class={classes!("lg:col-span-5", "bg-red-400")}><Education /></div>
+                    <div class={classes!("lg:col-span-6", "bg-red-400")}><Technologies /></div>
+                    <div class={classes!("lg:col-span-4", "bg-red-400")}><Awards /></div>
                 </div>
             </Container>
         </Body>
     }
 }
+

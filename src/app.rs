8a081398bfef4d82ac468153dc6306c78d2a4512
s1n7ax <srineshnisala@gmail.com>
2024-05-13 10:17:49 +0530

    feat(seo): add language attr to html

--- src/app.rs
+++ src/app.rs
@@ -12,13 +12,17 @@ pub fn App() -> impl IntoView {
 	provide_image_context();
 
 	view! {
-		<Stylesheet id="leptos" href="/pkg/my-website.css"/>
+		<Html
+			lang="en"
+		/>
 
 		<Meta charset="utf-8"/>
 		<Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<Meta name="author" content="Srinesha Nisala" />
 		<Meta name="description" content="I'm Srinesha Nisala and this is my portfolio"  />
 		<Meta name="theme-color" content="light" />
+
+		<Stylesheet id="leptos" href="/pkg/my-website.css"/>
 
 		// sets the document title
 		<Title text="Welcome to My Website"/>

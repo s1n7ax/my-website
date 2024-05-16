afba7109e5ad421bcaf099bfd3e052c1a96ef1da
s1n7ax <srineshnisala@gmail.com>
2024-05-16 21:34:48 +0530

    fix: tailwind does not pick up classes from conditional attrs in leptos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./style/input.css", "./src/**/*.rs"],
+  content: {
+    files: ["./style/input.css", "./src/**/*.rs"],
+    transform: {
+      rs: (content) => content.replace(/(?:^|\s)class:/g, " "),
+    },
+  },
+
   theme: {
     extend: {
       aspectRatio: {
